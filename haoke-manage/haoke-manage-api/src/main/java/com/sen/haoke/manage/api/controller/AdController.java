cbc8f590d4130a4e13a043617d69beedc17acbbf
Sen <12345678>
2020-03-03 23:40:38 +0800

    v1.0.0

--- haoke-manage/haoke-manage-api/src/main/java/com/sen/haoke/manage/api/controller/AdController.java
+++ haoke-manage/haoke-manage-api/src/main/java/com/sen/haoke/manage/api/controller/AdController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
